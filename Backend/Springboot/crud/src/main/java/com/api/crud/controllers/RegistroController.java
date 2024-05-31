fd78bf499c3cf04e07e51da56539aa25de976f38
David Arias <davidariascalderon04@gmail.com>
2024-05-30 23:57:31 -0500

    cambio en la bd, nombres de tablas en minusculas y tambien en el back

--- Backend/Springboot/crud/src/main/java/com/api/crud/controllers/RegistroController.java
+++ Backend/Springboot/crud/src/main/java/com/api/crud/controllers/RegistroController.java
@@ -70,14 +70,16 @@ public class RegistroController {
         usuarioModel.setFecha_creacion(ManejarFechas.obtenerFechaActual());
         usuarioModel.setActivo(true);
         userService.guardarUsuario(usuarioModel);
-
+        System.out.println("ENTRA A BUSCAR TIPO");
         Optional<UsuarioModel> usuarioAgregado = userService.buscarUsuario(usuario);
+        System.out.println("Encuentra el usuario");
         TipoUsuarioUsuarioModel tipo = new TipoUsuarioUsuarioModel();
         tipo.setUsuario_fk(usuarioAgregado.get().getId());
         Long idTipoCliente = tipoUsuarioService.obtenerIdCliente();
         tipo.setTipo_usuario_fk(idTipoCliente);
+        System.out.println(idTipoCliente);
         tipoUsuarioUsuarioService.guardarTipoUsuarioUsuario(tipo);
-
+        System.out.println("ENCONTRO TIPOS");
         EmailDTO email = new EmailDTO();
         email.setAsunto("Confirmación de cuenta");
         email.setDestinatario(correo);
